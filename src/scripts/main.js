32bf9c6189776636c2efd5ae22a266c5f839a63c
Samarash Eugene <samarasha95@gmail.com>
2025-02-25 16:25:58 +0200

    added check for validation data

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,12 +5,19 @@ const totalPopulation = document.querySelector('.total-population');
 const averagePopulation = document.querySelector('.average-population');
 
 let totalCount = 0;
+let validCount = 0;
 
 for (let i = 0; i < population.length; i++) {
-  totalCount += +population[i].innerText.split(',').join('');
+  const text = population[i].innerText.trim().replace(/,/g, '');
+  const number = Number(text);
+
+  if (!isNaN(number) && number > 0) {
+    totalCount += +number;
+    validCount++;
+  }
 }
 
-const average = totalCount / population.length;
+const average = validCount > 0 ? totalCount / population.length : 0;
 
 totalPopulation.innerHTML = totalCount.toLocaleString('en-US');
 averagePopulation.innerHTML = Math.round(average).toLocaleString('en-US');
