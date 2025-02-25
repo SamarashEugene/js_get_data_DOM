92c00cc6fa1e8ae46cacc8a359881af4c7dbacc0
Samarash Eugene <samarasha95@gmail.com>
2025-02-25 16:10:20 +0200

    tasks are completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const population = [...document.querySelectorAll('.population')];
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+let totalCount = 0;
+
+for (let i = 0; i < population.length; i++) {
+  totalCount += +population[i].innerText.split(',').join('');
+}
+
+const average = totalCount / population.length;
+
+totalPopulation.innerHTML = totalCount.toLocaleString('en-US');
+averagePopulation.innerHTML = Math.round(average).toLocaleString('en-US');
